884cf66cd3610887f5b3b5ec59565a789e87fe32
tenos200 <tim.enosedu@gmail.com>
2025-12-24 20:19:42 +0000

    Safety commit.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{
-    env,
+    env::{self, set_current_dir},
     io::{self, Write},
+    path::Path,
     process::Command,
 };
 
@@ -14,7 +15,7 @@ fn main() {
         env::set_current_dir(path).unwrap();
 
         let mut user_input = String::new();
-        print!("> ");
+        print!("$ ");
 
         io::stdout().flush().unwrap(); // Ensures it appears immediately.
         let bytes_read = io::stdin().read_line(&mut user_input).unwrap();
@@ -26,21 +27,32 @@ fn main() {
 
         // Unsure if this is supposed to be unwrap or we should check for err
         let mut parts = user_input.trim().split_whitespace();
-
         let command: String = parts.next().unwrap().to_string().to_lowercase();
+        dbg!(&parts);
         let args = parts.clone();
-
         let len = parts.count();
-
-        println!("{len}");
 
         if len == 0 && (command == "exit" || command == "quit") {
             break;
         }
 
-        // This should be when we don't recognise first word as cd etc.
-        let mut child = Command::new(command).args(args).spawn().unwrap();
+        // Here we want to try to insert the directory that user entered, as a string
+        // Here we want to change directory, in a safe way.
+        // We also need a way to check that its only 1 added argument not more
+        if command == "cd" {
+            let path_str = args.collect::<Vec<_>>().join(" ");
+            let new_dir = Path::new(&path_str);
+            // this how you change current dir just need to construt
+            // it from a string
+            match set_current_dir(new_dir) {
+                Ok(_) => println!("Successfully changed current dir"),
+                Err(_) => println!("Error: directory doesnt exists"),
+            };
+        } else {
+            // This should be when we don't recognise first word as cd etc.
+            let mut child = Command::new(command).args(args).spawn().unwrap();
 
-        child.wait().expect("Could not execute");
+            child.wait().expect("Could not execute");
+        }
     }
 }
