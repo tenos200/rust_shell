4614e9f45041a53762826c48658a768a1857a4eb
tenos200 <tim.enosedu@gmail.com>
2025-12-26 14:40:26 +0000

    fixed minor error handling of Command::new()

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    env::{self, current_dir},
+    env::{self},
     io::{self, Write},
     path::Path,
     process::Command,
@@ -7,6 +7,7 @@ use std::{
 
 // TODO: This program has way to many unwraps, these need to be removed and
 // handled appropriately
+// TODO: Handle unwrappings better.
 
 fn main() {
     // Start by setting current path to home directory
@@ -35,9 +36,6 @@ fn main() {
             break;
         }
 
-        // Here we want to try to insert the directory that user entered, as a string
-        // Here we want to change directory, in a safe way.
-        // We also need a way to check that its only 1 added argument not more
         if command == "cd" {
             let path_str = args.collect::<Vec<_>>().join(" ");
             let new_dir = Path::new(&path_str);
@@ -45,13 +43,16 @@ fn main() {
             // it from a string
             match env::set_current_dir(new_dir) {
                 Ok(_) => println!("Successfully changed current dir"),
-                Err(_) => println!("Error: directory doesnt exists"),
+                Err(_) => println!("cd: No such file or directory"),
             };
         } else {
             // This should be when we don't recognise first word as cd etc.
-            let mut child = Command::new(command).args(args).spawn().unwrap();
-
-            child.wait().expect("Could not execute");
+            match Command::new(command).args(args).spawn() {
+                Ok(mut child) => {
+                    child.wait().expect("Could not execute");
+                }
+                Err(_) => println!("tshell: command not found."),
+            }
         }
     }
 }
